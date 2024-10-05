02579118b4196933e2eb99afc190d71071d23ad7
Evelina <evelina@MacBook-Air-Evelina.local>
2024-10-05 11:36:04 +0300

    Update filter.py

--- moduls/filter.py
+++ moduls/filter.py
@@ -5,12 +5,12 @@ Contains auxiliary functions for filter_fastq
 
 def calculate_gc_content(sequence: str) -> float:
     gc_count = sequence.count("G") + sequence.count("C")
-    return (gc_count / len(sequence)) * 100 if len(sequence) > 0 else 0.0
+    if len(sequence) > 0:
+       return (gc_count / len(sequence)) * 100
+    return 0.0
 
 
 def calculate_average_quality(quality: str) -> float:
-    return (
-        sum(ord(char) - 33 for char in quality) / len(quality)
-        if len(quality) > 0
-        else 0.0
-    )
+    if len(quality) > 0:
+        return sum(ord(char) - 33 for char in quality) / len(quality)
+    return 0.0
